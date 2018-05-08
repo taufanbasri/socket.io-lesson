68b588770d67e9b5bcb8ab88c79b969435c0a05a
Taufan Prasetyo Basri <taufanprasetyobasri@gmail.com>
2018-05-08 10:23:39 +0700

    Basic config socket.io

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 server.listen(3000);
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
-})
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+    console.log('A connection was made.');
+});
